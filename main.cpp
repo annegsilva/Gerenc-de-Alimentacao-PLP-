9ce188d927b37efe279213ad2e5b4d3caa77a026
Leonardo Lima <leonardo.lima.silva@ccc.ufcg.edu.br>
2018-09-25 09:48:01 -0300

    bugs resolvidos :)

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <iomanip>
 #include <fstream>
+#include <sstream> 
 
 using namespace std;
 
@@ -20,11 +21,13 @@ struct Usuario {
 	tipos_dieta td;
 };
 
+// Lista dos metodos do gerenciador
 void cadastro();
 void menu(Usuario usuarios[]);
 void lerUsuarios(Usuario usuarios[]);
 void salvarUsuario(string usuario);
-bool login(Usuario usuarios[]);
+void login(Usuario usuarios[]);
+void mostrarUsuarios(Usuario usuarios[]);
 const vector<string> explode(const string& s, const char& c);
 
 int num_usuarios = 0;
@@ -35,38 +38,33 @@ int main(int argc, char const *argv[]) {
 	return 0;
 }
 
-mostrarUsuarios(Usuario usuarios[]) {
-	int i;
-	for (i = 0; i < num_usuarios; i++) {
-		cout << usuarios[i].username << endl;
-	}
-}
-
+// Metodo inicial. menu
 void menu(Usuario usuarios[]) {
 	lerUsuarios(usuarios);
-	mostrarUsuarios(usuarios);
 	cout << "Bem Vindo ao Gerenciador de Alimentacao! -----\n";
 	int opcao;
-	
+
 	do {
 		switch (opcao) {
 			case 1:
 			login(usuarios);
 			break;
-      
+
 			case 2:
 			cadastro();
 			break;
 		}
+	lerUsuarios(usuarios);
 	cout << "\nEscolha uma opcao:";
 	cout << "\n1 - Realizar Login";
 	cout << "\n2 - Realizar Cadastro";
-	cout << "\n3 - Sair\n\n"
+	cout << "\n3 - Sair\n\n";
 	cin >> opcao;
 	} while (opcao != 3);
 }
 
-bool login(Usuario usuarios[]) {
+
+void login(Usuario usuarios[]) {
 	string username;
 	string senha;
 	int i;
@@ -75,17 +73,19 @@ bool login(Usuario usuarios[]) {
 	cin >> username;
 	cout << "\nSenha: ";
 	cin >> senha;
-
+	
 	for (i = 0; i < num_usuarios; i++) {
 		if (usuarios[i].username == username && usuarios[i].senha == senha) {
-			cout << "encontrei o usuario e estou logaado";
-			return true;
+			cout << "encontrei o usuario e estou logado";
 		}
 	}
-	
-	return false;
+
+	// abaixo fluxo de execucao após logar ???
+	// funcionalidades 2 e 3
 }
 
+
+// menu de cadastro: coleta inicial de todas as informações do mesmo
 void cadastro() {
 
 	string usuario[9];
@@ -97,7 +97,7 @@ void cadastro() {
 
 	cout << "\nSobre Voce";
 	cout << "\nNome: ";
-	cin >> usuario[2];
+	getline(cin.ignore(),usuario[2]);
 	cout << "\nIdade: ";
 	cin >> usuario[3];
 	cout << "\nPeso (kg): ";
@@ -115,6 +115,9 @@ void cadastro() {
 	cout << "\n(3) Dieta dos pontos\n";
 	cin >> usuario[8];
 
+	/*
+	Ao cadastrar cria string pronta para ser salva diretamente no arquivo
+	*/
 	string usuario_g;
 	int i;
 	for (i = 0; i < 9; i++) {
@@ -126,19 +129,19 @@ void cadastro() {
 	cout << "\nCadastro Realizado com sucesso!!!\n";
 }
 
-// Daqui pra baixo eh tudo arquivo
-
+// Leitura dos usuarios gravados em usuarios.txt
 void lerUsuarios(Usuario usuarios[]) {
 	string line;
 	ifstream myfile ("usuarios.txt");
-	
+
 	if (myfile.is_open()) {
+		
 		int count;
 		while (!myfile.eof()) {
 			count = 0			;
 			getline(myfile,line);
 			vector<string> usuario{explode(line, ';')};
-		
+
 			for (auto n:usuario) {
 				if (n != "fim") {
 					usuario[count] = n;
@@ -146,10 +149,30 @@ void lerUsuarios(Usuario usuarios[]) {
 				}
 			}
 
+			/*
+			Cria novo usuario e o adiciona no fluxo de execução do programa
+			*/
 			Usuario novoUsuario;
 			novoUsuario.username = usuario[0];
 			novoUsuario.senha = usuario[1];
 			novoUsuario.nome = usuario[2];
+			int idade = 0;
+			stringstream converter(usuario[3]); 
+			converter >> idade;
+			novoUsuario.idade = idade;
+			novoUsuario.peso = strtof((usuario[4]).c_str(),0);
+			novoUsuario.altura = strtof((usuario[5]).c_str(),0);
+			novoUsuario.circunferencia_abdominal = strtof((usuario[6]).c_str(),0);
+			novoUsuario.quadril = strtof((usuario[7]).c_str(),0);
+
+			if(usuario[8] == "1"){
+				novoUsuario.td = tipos_dieta::ganho_de_massa;
+			} else if(usuario[8] == "2"){
+				novoUsuario.td = tipos_dieta::emagrecimento;
+			} else if(usuario[8] == "3"){
+				novoUsuario.td = tipos_dieta::dieta_dos_pontos;
+			}
+
 			usuarios[num_usuarios] = novoUsuario;
 			num_usuarios ++;
 		}
@@ -159,10 +182,11 @@ void lerUsuarios(Usuario usuarios[]) {
 	}
 }
 
+// Salva usuario em usuarios.txt
 void salvarUsuario(string usuario) {
 	std::ofstream Hypnos_FILE;
 	Hypnos_FILE.open("usuarios.txt", std::ios::app);
-	
+
 	if (Hypnos_FILE.is_open()) {
 		Hypnos_FILE << endl << usuario;
 	} else {
@@ -171,21 +195,29 @@ void salvarUsuario(string usuario) {
 	}
 }
 
+// Split para auxiliar na quebra de tokens da entrada (arquivo)
 const vector<string> explode(const string& s, const char& c) {
 	string buff{""};
 	vector<string> v;
 
 	for(auto n:s){
-		if (n != c) 
+		if (n != c)
 			buff+=n;
 		else if (n == c && buff != "") {
-			v.push_back(buff); 
+			v.push_back(buff);
 			buff = "";
 		}
 	}
-	
-	if (buff != "") 
+
+	if (buff != "")
 		v.push_back(buff);
 
 	return v;
+}
+
+void mostrarUsuarios(Usuario usuarios[]) {
+	int i;
+	for (i = 0; i < num_usuarios; i++) {
+		cout << usuarios[i].idade << endl;
+	}
 }
