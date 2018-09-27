b6da18078bd79214428342f5b0bc0ff3e90f6471
larissaamorim <34272746+larissaamorim@users.noreply.github.com>
2018-09-26 22:17:07 -0300

    func2-calculo dietas

--- func2.cpp
+++ func2.cpp
@@ -16,18 +16,18 @@ struct Usuario {
 	float quadril;
 	tipos_dieta td;
 	
-	int proteina;
-	int carboidrato;
-	int gorduras;
-	int vitaminas;
-	int minerais;
+	float proteina;
+	float carboidrato;
+	float gorduras;
 	int pontos;
 
 	void tipoDieta() {
-		if (td == ganho_de_massa || td == emagrecimento) {
+		if (td == ganho_de_massa) {
 			menu1();
-		} else {
+		} else if(td == emagrecimento){
 			menu2();
+		}else{
+			menu3();
 		}
 	}
 	
@@ -39,37 +39,93 @@ struct Usuario {
 		cin >> carboidrato;
 		cout << "Gorduras: ";
 		cin >> gorduras;
-		cout << "Vitaminas: ";
-		cin >> vitaminas;
-		cout << "Minerais: ";
-		cin >> minerais;
+
 		
-		retorno1();
+		retorno1(proteina, carboidrato, gorduras);
 	}
 	
-	void menu2() {
+	void menu2(){
+		cout << "Informe a quantidade dos seguintes nutrientes em gramas (g): \n";
+		cout << "Proteína: ";
+		cin >> proteina;
+		cout << "Carboidrato: ";
+		cin >> carboidrato;
+		cout << "Gorduras: ";
+		cin >> gorduras;
+		
+		retorno2(proteina, carboidrato, gorduras);
+	}
+	
+	void menu3() {
 		cout << "Informe a quantidade de pontos da sua refeição: ";
 		cin >> pontos;
 		
-		retorno2();
+		retorno3(pontos);
 	}
 	
-	void retorno1() {
-		cout << "sua dieta esta x \n";
+	void retorno1(float &proteina, float &carboidrato, float &gorduras) {
+		if((proteina >= 1.4 && proteina <= 2) && carboidrato >= 150 && gorduras <= 22.2){
+			cout << "Muito bem! A quantidade de proteínas, carboidratos e gorduras estão adequados.";
+		}
+		
+		else if (gorduras > 22.2){
+			cout << "Olá! É necessário diminuir a quantidade de gorduras (no máximo 22.2 gramas por dia) para um melhor resultado.";
+		}
+		
+		else if (carboidrato < 150){
+			cout << "Olá! É necessário aumentar a quantidade de carboidratos (no mínimo 150 gramas por dia) para um melhor resultado.";
+		}	
+		else if (proteina > 2){
+			cout << "Olá! É necessário diminuir a quantidade de proteínas (no máximo 2 gramas por dia) para um melhor resultado.";
+		}
+		else if (proteina < 1.4){
+			cout << "Olá! É necessário aumentar a quantidade de proteínas (no mínimo 1.4 gramas por dia) para um melhor resultado.";
+		}
 	}
 	
-	void retorno2() {
+	void retorno2(float &proteina, float &carboidrato, float &gorduras) {
+		if((proteina >= 0.8 && proteina <= 1.4) && carboidrato >= 100 && gorduras <= 22.2){
+			"Muito bem! A quantidade de proteínas, carboidratos e gorduras estão adequados.";
+		
+		}
+		
+		else if (gorduras > 22.2){
+			cout << "Olá! É necessário diminuir a quantidade de gorduras (no máximo 22.2 gramas por dia) para um melhor resultado.";
+		}
+		
+		else if (carboidrato < 100){
+			cout << "Olá! É necessário aumentar a quantidade de carboidratos (no mínimo 100 gramas por dia) para um melhor resultado.";
+		}	
+		else if (proteina > 1.4){
+			cout << "Olá! É necessário diminuir a quantidade de proteínas (no máximo 1.4 gramas por dia) para um melhor resultado.";
+
+		}
+		else if (proteina < 0.8){
+			cout << "Olá! É necessário aumentar a quantidade de proteínas (no mínimo 0.8 gramas por dia) para um melhor resultado.";
+		}
+		
+		
+		
 		cout << "sua dieta esta y \n";
+	}
+	
+	void retorno3(int &pontos){
+		// NATHALYA	
 	}
 
 };
 
 int main() {
 	Usuario user;
-	Usuario user2;
+	Usuatia user2;
+	Usuario user3;
 	user.td = ganho_de_massa;
-	user2.td = dieta_dos_pontos;
+	user2.td = dieta_para_emagreciento;
+	user3.td = dieta_dos_pontos;
 	user.tipoDieta();
 	user2.tipoDieta();
+	user3.tipoDieta();
+	
+	
 	return 0;
 }
