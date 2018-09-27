0fed6aaf3fc3a3589566b5f34043eed714dacc34
annegsilva <30444315+annegsilva@users.noreply.github.com>
2018-09-26 21:37:46 -0300

    Update func3.cpp

--- func3.cpp
+++ func3.cpp
@@ -5,10 +5,28 @@
 #include <fstream>
 #include <math.h>
 
-#include "usuario.h"
-
 
 using namespace std;
+enum tipos_dieta { ganho_de_massa, emagrecimento, dieta_dos_pontos };
+struct Usuario {
+	// Funcionalidade 1
+	string username;
+	string senha;
+	string nome;
+	int idade;
+	char sexo;
+	float peso[10];
+	float altura;
+	float circunferencia_abdominal[10];
+	float quadril[10];
+	tipos_dieta td;
+	// primeiro indice livre
+	int index;
+
+	// Funcionalidade 3
+	// matriz que salva em tres colunas 1: data, 2: imc, 3: rcq.
+float indices[10][3];
+};
 
 //Funcionalidade 3
 // As funcionalidades devem ser trabalhadas considerando um array de usuaraios
