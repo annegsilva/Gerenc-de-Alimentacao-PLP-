f6f0104f22517a041871c593033d9e1016729289
nthlua <nthlyaraissa@gmail.com>
2018-09-27 22:24:09 -0300

    atualizando os dislplays de funcs.cpp

--- display.cpp
+++ display.cpp
@@ -36,3 +36,17 @@ void dietsMenu() {
 	cout << "(2) Emagrecimento" << endl;
 	cout << "(3) Dieta dos pontos" << endl;
 }
+
+//Saidas das funcionalidades 2 e 3 para o funcs.cpp
+# define ADEQUATE_P "Sua refeição tem a quantidade: IDEAL DE PROTEÍNA!"<< endl;
+# define LESS_P "Sua refeição tem a quantidade: MENOR QUE A IDEAL DE PROTEÍNA!"<< endl;
+# define OVER_P "Sua refeição tem a quantidade: SUPERIOR QUE A NECESSÁRIA DE PROTEÍNA! \nLembre-se que o nosso corpo tem um limite máximo de absorção."<< endl;
+
+# define ADEQUATE_C "Sua refeição tem a quantidade: IDEAL DE CARBOIDRATOS!" << endl;
+# define LESS_C "Sua refeição tem a quantidade: MENOR QUE A IDEAL DE CARBOIDRATOS! \nLembre-se que para o ganho de massa muscular o consumo de carboidratos também é importante!" << endl;
+
+# define ADEQUATE_G "Sua refeição tem a quantidade: IDEAL DE GORDURA!" << endl;
+# define LESS_G "Sua refeição tem a quantidade: MENOR QUE A IDEAL DE GORDURA! \nLembre-se que o consumo de gorduras BOAS também é importante!" << endl;
+
+# define ADEQUATE_PO "Sua refeição tem uma boa quantidade de PONTOS" << endl;
+# define LESS_PO "Sua refeição tem a quantidade: MENOR QUE A IDEAL DE PONTOS! \nLembre-se em consumir seus pontos de forma equilibrada." << endl;
