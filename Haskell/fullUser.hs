1bbd8f467e169474c06b42d3e2c2b81d59b7642f
nthlua <nthlyaraissa@gmail.com>
2018-10-22 18:41:33 -0300

    Formato quase final de User

--- Haskell/fullUser.hs
+++ Haskell/fullUser.hs
@@ -1,70 +1,74 @@
-
 module User where
 
--- Informacoes do gerais do usuario
+-- Informacoes gerais do usuario
 type Username = String
 type Password = String
 type Nome = String
 type Idade = Int
 type Sexo = String
-type Dieta = Int
 --Informacoes Fisicas do Usuario
-type Peso = Float
 type Altura = Float
-type Circunf = Float
-type Quadril = Float
+type Peso = [Float]
+type Circunf = [Float]
+type Quadril = [Float]
 --Informacoes diarias do usuario
+type Dieta = Int
 type Proteina = [Float]
 type Carboidrato = [Float]
 type Gordura = [Float]
 type Pontos = [Float]
 -- Informacoes armazenadas (como uma Heap, onde cada indice equivale a um tipo de medida salva)
-type indices =[Float]
-type User = (Username, Password, Nome, Idade, Sexo, Peso, Altura, Circunf, Quadril, Dieta)
+type Indices =[Float]
+
+-- estrutura de acesso ao User
+type User = (Username, Password, Nome, Idade, Sexo, Altura, Peso, Circunf, Quadril, Dieta, Proteina, Carboidrato, Gordura, Pontos, Indices)
 
 -- Estrutura de Usuario
 
--- Retorna o username do usuario
+-- (1) Retorna o Username do usuario
 getUsername :: User -> String
-getUsername (username,_,_,_,_,_,_,_,_,_) = username
+getUsername (username,_,_,_,_,_,_,_,_,_,_,_,_,_,_) = username
 
--- Retorna o password do usuario
+-- (2) Retorna o Password do usuario
 getPassword :: User -> String
-getPassword (_,password,_,_,_,_,_,_,_,_) = password
+getPassword (_,password,_,_,_,_,_,_,_,_,_,_,_,_,_) = password
 
--- Retorna o nome do usuario
+-- (3) Retorna o Nome do usuario
 getNome :: User -> String
-getNome (_,_,nome,_,_,_,_,_,_,_) = nome
+getNome (_,_,nome,_,_,_,_,_,_,_,_,_,_,_,_) = nome
 
--- Retorna a idade do usuario
+-- (4) Retorna a Idade do usuario
 getIdade :: User -> Int
-getIdade (_,_,_,idade,_,_,_,_,_,_) = idade
+getIdade (_,_,_,idade,_,_,_,_,_,_,_,_,_,_,_) = idade
 
--- Retorna o sexo do usuario
+-- (5) Retorna o Sexo do usuario
 getSexo :: User -> String
-getSexo (_,_,_,_,sexo,_,_,_,_,_) = sexo
+getSexo (_,_,_,_,sexo,_,_,_,_,_,_,_,_,_,_) = sexo
 
--- Retona o peso do usuario
-getPeso :: User -> Int -> Float
-getPeso (_,_,_,_,_,peso,_,_,_,_) = peso
-
--- Retorna a altura do usuario
+-- (6) Retorna a Altura do usuario
 getAltura :: User -> Float
-getAltura (_,_,_,_,_,_,altura,_,_,_) = altura
+getAltura (_,_,_,_,_,altura,_,_,_,_,_,_,_,_,_) = altura
 
--- Retorna a medida de circunferencia abdominal do usuario 
-getCircunf :: User -> Float
-getCircunf (_,_,_,_,_,_,_,circunf,_,_) = circunf
+-- (7) Retona a lista de Pesos do usuario
+getPeso :: User -> [Float]
+getPeso (_,_,_,_,_,_,peso,_,_,_,_,_,_,_,_) = peso
 
--- Retorna a medida do quadril do usuario
-getQuadil :: User -> Float
-getQuadil (_,_,_,_,_,_,_,_,quadril,_) = quadril
+-- (8) Retorna a lista das medidas de Circunferencia Abdominal do usuario 
+getCircunf :: User -> [Float]
+getCircunf (_,_,_,_,_,_,_,circunf,_,_,_,_,_,_,_) = circunf
 
--- Retorna o tipo de dieta do usuario
+-- (9) Retorna a lista das medidas do Quadril do usuario
+getQuadil :: User -> [Float]
+getQuadil (_,_,_,_,_,_,_,_,quadril,_,_,_,_,_,_) = quadril
+
+-- (10) Retorna o tipo de Dieta do usuario
 getDieta :: User -> Int
-getDieta (_,_,_,_,_,_,_,_,_,dieta) = dieta
+getDieta (_,_,_,_,_,_,_,_,_,dieta,_,_,_,_,_) = dieta
 
---Funcionalidade 2
---Metodo para avaliar a porcao de proteina da dieta tipo (1)
+-- (11) Retorna a lista dos valores das Proteinas do usuario
+getProt :: User -> [Float]
+getProt (_,_,_,_,_,_,_,_,proteina,_,_,_,_,_,_) = proteina
 
-proteina_tipo1 :: User -> Float -> Int
+-- Retona o valor da medida mais recente do usuario
+getMedidaAtual :: [Float] -> Float
+getMedidaAtual (x:xs) = x
