ed62559354016e04817d748b7149d2aa2980023f
Leonardo Lima <leonardo.lima.silva@ccc.ufcg.edu.br>
2018-10-22 18:41:54 -0300

    Concluindo funcionalidade de login e cadastro

--- Haskell/Files.hs
+++ Haskell/Files.hs
@@ -5,20 +5,36 @@ import System.IO.Error
 import System.IO
 import User
 
+login :: String -> String -> IO User
+login username password = do
+ user <- readUser username
+ if (user == ("","","",0,0,0,0,0,0)) then do
+  putStrLn("Usuario inexistente!")
+  return ("","","",0,0,0,0,0,0)
+ else if (getPassword user == password) then do
+  putStrLn("Logado com Sucesso!")
+  return user
+ else do
+  putStrLn("Senha Incorreta!") 
+  return ("","","",0,0,0,0,0,0)
+
+logout :: User -> IO()
+logout user = do
+ saveUser user 
+ putStrLn("Usuario deslogado!")
+
 readUser :: String -> IO User
 readUser username = do
 {catch (ler_arquivo) tratar_erro;}
  where
  ler_arquivo = do
  {
-  readData <- readFile ("users/"++username++".txt")
-  let user = ((read readData) :: User)
-  let oi = 10
+  readData <- readFile ("users/"++username++".txt");
+  return (read readData)
  }
  tratar_erro erro = if isDoesNotExistError erro
   then do
  {
-  putStrLn("Usuario não existente!");
   return ("","","",0,0,0,0,0,0);
  }
  else ioError erro
