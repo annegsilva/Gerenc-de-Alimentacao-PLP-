dbb0ebdacf9521ad3aee6492f6bd8f32867188ba
annegsilva <30444315+annegsilva@users.noreply.github.com>
2018-11-13 11:14:39 -0300

    add relatorio_indices

--- Logico/indices.pl
+++ Logico/indices.pl
@@ -9,13 +9,13 @@ calcula_imc(Peso, Altura, IMC) :-
 	IMC is Peso / (Altura**2).
 
 aval_imc(IMC) :-
-	(IMC < 17-> writeln("MUITO ABAIXO DO PESO");
-	IMC >= 17, IMC < 18.5 -> writeln("ABAIXO DO PESO");
-	IMC >= 18.5, IMC < 24.5 -> writeln("PESO NORMAL");
-	IMC >= 24.5, IMC < 30 -> writeln("ACIMA DO PESO");
-	IMC >= 30, IMC < 35 -> writeln("OBESIDADE I");
-	IMC >= 35, IMC < 40 -> writeln("OBESIDADE II (severa)");
-	writeln("OBESIDADE III (mórbida)")).
+	(IMC < 17-> write("MUITO ABAIXO DO PESO");
+	IMC >= 17, IMC < 18.5 -> write("ABAIXO DO PESO");
+	IMC >= 18.5, IMC < 24.5 -> write("PESO NORMAL");
+	IMC >= 24.5, IMC < 30 -> write("ACIMA DO PESO");
+	IMC >= 30, IMC < 35 -> write("OBESIDADE I");
+	IMC >= 35, IMC < 40 -> write("OBESIDADE II (severa)");
+	write("OBESIDADE III (mórbida)")).
 
 calcula_rcq(Cintura, Quadril, RCQ) :-
 	RCQ is Cintura / Quadril.
@@ -29,6 +29,16 @@ aval_rcq(RCQ, Sexo) :-
 	RCQ > 1 -> writeln("ALTO RISCO DE SAÚDE");
 	writeln("MODERADO RISCO DE SAÚDE"))).	
 	
+relatorio_indices([X|XS], [Y|YS], [Z|ZS], Sexo) :-
+	write("\n----- Seus índices da data "), write(X), writeln(" são -----\n"),
+	write("IMC ...: "), writeln(Y), 
+	write("Análise: "), aval_imc(Y),
+	write("\nRCQ ...: "), writeln(Z), 
+	write("Análise: "), aval_rcq(Z, Sexo),
+	(XS == [] -> write("");
+	relatorio_indices(XS, YS, ZS, Sexo)).
+	
+	
 main:-
 	lerInt(Peso),
 	lerInt(Altura),
@@ -38,4 +48,6 @@ main:-
 	writeln(RCQ),
 	aval_imc(IMC),
 	aval_rcq(RCQ, "F"),
-	aval_rcq(RCQ, "M").
+	aval_rcq(RCQ, "M"),
+	read_line_to_string(user_input,Sexo),
+	relatorio_indices(["15/10/18","09/11/18"],[17, 20], [0.7, 0.8], Sexo).
