04d09ef1a6dd39d8999ab005110272d6e9b8e79a
Leonardo Lima <leonardo.lima.silva@ccc.ufcg.edu.br>
2018-11-19 15:41:14 -0300

    Atualizando informações do usuario

--- Logico/file.pl
+++ Logico/file.pl
@@ -1,26 +1,29 @@
-:- module(file, [salvarUsuario/10,login/3]).
+:- module(file, [salvarUsuario/8,login/3]).
 
 use_module(util).
 
-%! Usuario padrão
-%! Username, Senha, Sexo, Altura, Peso, Cintura, Quadril, Dieta, Proteina, Carboidrato, Gordura, Pontos, [DataUpdate], [IMC], [RCQ]
-usuario("leonardo","123","Leonardo",19,1.93,82.1).
+%! Username, Senha, Sexo, Altura, Peso, Cintura, Quadril, Dieta,
+%! Proteina, Carboidrato, Gordura, Pontos, DataUpdate, IMC, RCQ 
 
-salvarUsuario(Username,Password,PrimeiroNome,Idade,Sexo,Altura,Peso,Cintura,Quadril,Dieta):-
-		downcase_atom(PrimeiroNome,Nome),
+salvarUsuario(Username,Password,Sexo,Altura,Peso,Cintura,Quadril,Dieta):-
 		string_concat(Username,".abc",NomeArquivo),
 		string_concat("users/",NomeArquivo,NomeArquivoFinal),
 		open(NomeArquivoFinal,write,Stream),
 		add_in_list([],Username,UserFile),
 		add_in_list(UserFile,Password,SenhaFile),
-		add_in_list(SenhaFile,Nome,NomeFile),
-		add_in_list(NomeFile,Idade,IdadeFile),
-		add_in_list(IdadeFile,Sexo,SexoFile),
+		add_in_list(SenhaFile,Sexo,SexoFile),
 		add_in_list(SexoFile,Altura,AlturaFile),
-		add_in_list(AlturaFile,[Peso],PesoFile),
-		add_in_list(PesoFile,[Cintura],CinturaFile),
-		add_in_list(CinturaFile,[Quadril],QuadrilFile),
-		add_in_list(QuadrilFile,Dieta,File),
+		add_in_list(AlturaFile,Peso,PesoFile),
+		add_in_list(PesoFile,Cintura,CinturaFile),
+		add_in_list(CinturaFile,Quadril,QuadrilFile),
+		add_in_list(QuadrilFile,Dieta,DietaFile),
+		add_in_list(DietaFile,0,ProteinaFile),
+		add_in_list(ProteinaFile,0,CarboidratoFile),
+		add_in_list(CarboidratoFile,0,GorduraFile),
+		add_in_list(GorduraFile,0,PontosFile),
+		add_in_list(PontosFile,[],DataUpdateFile),
+		add_in_list(DataUpdateFile,[],IMCFile),
+		add_in_list(IMCFile,[],File),
 		write(Stream,File),
 		write(Stream,"."),
 		close(Stream),
@@ -35,13 +38,11 @@ lerUsuario(Username,Usuario):-
 	read_term(Stream, L,[]),
 	nth0(0,L,Username1),
 	nth0(1,L,Password1),
-	nth0(2,L,Nome1),
+	nth0(2,L,Sexo1),
 	convertArrayCharToStr(Username1,NewUsername),
 	convertArrayCharToStr(Password1,NewPassword),
-	convertArrayCharToStr(Nome1,NewNome),
 	setUsername(L,NewUsername,Usuario1),
-	setSenha(Usuario1,NewPassword,Usuario2),
-	setNome(Usuario2,NewNome,UsuarioFinal),
+	setSenha(Usuario1,NewPassword,UsuarioFinal),
 	close(Stream),
 	Usuario = UsuarioFinal.
 
